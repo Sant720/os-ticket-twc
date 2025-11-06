a5945e6be393bd83bde505af16fbb0995444d59f
Santiago Bolivar <santiago.bolivar7@gmail.com>
2025-11-05 22:17:01 -0500

    add files

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,13 @@ import nextra from 'nextra'
  
 // Set up Nextra with its configuration
 const withNextra = nextra({
-  // ... Add Nextra-specific options here
+  latex: true,
+  search: {
+    codeblocks: false
+  }
 })
  
 // Export the final Next.js config with Nextra included
 export default withNextra({
-  // ... Add regular Next.js options here
+  reactStrictMode: true,
 })
